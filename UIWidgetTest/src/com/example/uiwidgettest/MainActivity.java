2b46ee3939630ee164b778a05899b922cee6b490
hp <511022248@qq.com>
2015-11-10 16:37:32 +0800

    自定义控件

--- UIWidgetTest/src/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/src/com/example/uiwidgettest/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -21,61 +22,71 @@ public class MainActivity extends Activity implements OnClickListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
-		Button button = (Button) findViewById(R.id.Login);
-		button.setOnClickListener(this);
+//		Button button = (Button) findViewById(R.id.Login);
+//		button.setOnClickListener(this);
+//		Button editbutton = (Button) findViewById(R.id.title_edit);
+//		editbutton.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch (v.getId()) {// 布局内可能有多个按钮
-		case R.id.Login:
+//		switch (v.getId()) {// 布局内可能有多个按钮
+//		case R.id.Login:
 			// ImageView imageView = (ImageView)findViewById(R.id.image_view);
 			// imageView.setImageResource(R.drawable.hello);
-//			ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
-//			if (progressBar.getVisibility() == View.GONE) {
-//				progressBar.setVisibility(View.VISIBLE);
+			// ProgressBar progressBar = (ProgressBar)
+			// findViewById(R.id.progress_bar);
+			// if (progressBar.getVisibility() == View.GONE) {
+			// progressBar.setVisibility(View.VISIBLE);
+			// } else
+			// progressBar.setVisibility(View.GONE);
+			// int progress = progressBar.getProgress();
+			// progress+=10;
+			// progressBar.setProgress(progress);
+			// AlertDialog.Builder dialog = new
+			// AlertDialog.Builder(MainActivity.this);
+			// dialog.setTitle("This is Dialog");
+			// dialog.setMessage("Something important.");
+			// dialog.setCancelable(false);
+			// dialog.setPositiveButton("OK", new
+			// DialogInterface.OnClickListener(){
+			// @Override
+			// public void onClick(DialogInterface dialog,int which){
+			//
+			// }
+			// });
+			// dialog.setNegativeButton("Cancel", new
+			// DialogInterface.OnClickListener(){
+			// @Override
+			// public void onClick(DialogInterface dialog,int which){
+			//
+			// }
+			// });
+			// dialog.show();
+			// ProgressDialog progressDialog = new
+			// ProgressDialog(MainActivity.this);
+			// progressDialog.setTitle("This is ProgressDialog");
+			// progressDialog.setMessage("Loading...");
+			// progressDialog.setCancelable(true);
+			// progressDialog.show();
+
+//			TextView account = (TextView) findViewById(R.id.account);
+//			TextView password = (TextView) findViewById(R.id.password);
+//			if (account.getText().toString().equals("admin") && password.getText().toString().equals("123456")) {
+//				Toast.makeText(MainActivity.this, "login success!", Toast.LENGTH_SHORT).show();
 //			} else
-//				progressBar.setVisibility(View.GONE);
-//			int progress = progressBar.getProgress();
-//			progress+=10;
-//			progressBar.setProgress(progress);
-//			AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-//			dialog.setTitle("This is Dialog");
-//			dialog.setMessage("Something important.");
-//			dialog.setCancelable(false);
-//			dialog.setPositiveButton("OK", new DialogInterface.OnClickListener(){
-//				@Override
-//				public void onClick(DialogInterface dialog,int  which){
-//					
-//				}
-//			});
-//			dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
-//				@Override
-//				public void onClick(DialogInterface dialog,int  which){
-//					
-//				}
-//			});
-//			dialog.show();
-//			ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
-//			progressDialog.setTitle("This is ProgressDialog");
-//			progressDialog.setMessage("Loading...");
-//			progressDialog.setCancelable(true);
-//			progressDialog.show();
-			
-			TextView account = (TextView)findViewById(R.id.account);
-			TextView password = (TextView)findViewById(R.id.password);
-			if(account.getText().toString().equals("admin")&&password.getText().toString().equals("123456")){
-				Toast.makeText(MainActivity.this, "login success!", Toast.LENGTH_SHORT).show();
-			} else
-				Toast.makeText(MainActivity.this, "login failed!", Toast.LENGTH_SHORT).show();
-				
-			
-			
-			break;
-		default:
-			break;
-		}
+//				Toast.makeText(MainActivity.this, "login failed!", Toast.LENGTH_SHORT).show();
+//
+//			break;
+//		case R.id.title_edit:
+//			Toast.makeText(MainActivity.this, "you clicked edit button", Toast.LENGTH_SHORT).show();
+//			break;
+//		default:
+//			break;
+//		}
+
 	}
 
 	@Override
