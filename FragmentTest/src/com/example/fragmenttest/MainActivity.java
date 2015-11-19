4bd307b7b0807ecfc310172a0dabe59ee70ef6da
hp <511022248@qq.com>
2015-11-19 16:14:37 +0800

    update

--- FragmentTest/src/com/example/fragmenttest/MainActivity.java
+++ FragmentTest/src/com/example/fragmenttest/MainActivity.java
@@ -10,32 +10,31 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		Button button = (Button)findViewById(R.id.button);
-		button.setOnClickListener(this);
+		Button button = (Button) findViewById(R.id.button);
+		// button.setOnClickListener(this);
 	}
 
-	
-	public void onClick(View v){
-		switch(v.getId()){
-		case R.id.button:
-			AnotherRightFragment fragment = new AnotherRightFragment();
-			FragmentManager fragmentManager = getFragmentManager();
-			FragmentTransaction transaction = fragmentManager.beginTransaction();
-			transaction.replace(R.id.right_layout, fragment);
-			transaction.addToBackStack(null);
-			transaction.commit();
-			break;
-		default:
-			break;
-		}
-	}
-	
+	// public void onClick(View v){
+	// switch(v.getId()){
+	// case R.id.button:
+	// AnotherRightFragment fragment = new AnotherRightFragment();
+	// FragmentManager fragmentManager = getFragmentManager();
+	// FragmentTransaction transaction = fragmentManager.beginTransaction();
+	// transaction.replace(R.id.right_layout, fragment);
+	// transaction.addToBackStack(null);
+	// transaction.commit();
+	// break;
+	// default:
+	// break;
+	// }
+	// }
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
