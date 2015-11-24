cc24112dc0224e0d8bbf44eb3c10722697a08dfb
hp <511022248@qq.com>
2015-11-24 18:50:43 +0800

    有序广播，可以阻止进一步广播

--- BroadcastTest/src/com/example/broadcasttest/MainActivity.java
+++ BroadcastTest/src/com/example/broadcasttest/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v){
 				Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
-				sendBroadcast(intent);
+				sendOrderedBroadcast(intent,null);
 			}
 		});
 	}
