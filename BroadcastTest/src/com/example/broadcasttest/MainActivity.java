0bb882e2ca0f747ae7c9baca196083d4a1ed00e1
hp <511022248@qq.com>
2015-11-24 18:00:20 +0800

    NetWork Changes BroadcastTest

---
+++ BroadcastTest/src/com/example/broadcasttest/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.broadcasttest;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	
+	private IntentFilter intentFilter;
+	private NetWorkChangeReceiver networkChangeReceiver;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		intentFilter = new IntentFilter();
+		intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+		networkChangeReceiver = new NetWorkChangeReceiver();
+		registerReceiver(networkChangeReceiver,intentFilter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	class NetWorkChangeReceiver extends BroadcastReceiver{
+		@Override
+		public void onReceive(Context context,Intent intent){
+			Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		unregisterReceiver(networkChangeReceiver);
+	}
+	
+}
